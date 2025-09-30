c0452402864b37b9137d909274648ee9764a6f0d
Ikechukwu-Patrick <patrickikechukwukalukalu@gmail.com>
2025-09-30 14:30:45 +0100

    Update Dockerfile for Maven build on Render

--- src/test/java/org/example/services/WalletServiceImplTest.java
+++ src/test/java/org/example/services/WalletServiceImplTest.java
@@ -1,27 +1,27 @@
-// package org.example.services;
+package org.example.services;
 
-// import org.example.dtos.request.CreateWalletRequest;
-// import org.example.dtos.response.CreateWalletResponse;
-// import org.example.model.Wallet;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.example.dtos.request.CreateWalletRequest;
+import org.example.dtos.response.CreateWalletResponse;
+import org.example.model.Wallet;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// import static org.junit.jupiter.api.Assertions.*;
-// @SpringBootTest
-// public class WalletServiceImplTest {
-//     @Autowired
-//     private WalletService walletService;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+public class WalletServiceImplTest {
+    @Autowired
+    private WalletService walletService;
 
-//     @Test
-//     public void testCreateWallet() {
-// //        CreateWalletRequest request = new CreateWalletRequest();
-// //        request.setId(1L);
-// //        request.setAmount(0.00);
-// //        request.setBalance(0.00);
-// //        CreateWalletResponse response = walletService.createWallet(request);
-// //        assertNotNull(response);
-// //        assertNotNull(response.getId());
-//     }
+    @Test
+    public void testCreateWallet() {
+//        CreateWalletRequest request = new CreateWalletRequest();
+//        request.setId(1L);
+//        request.setAmount(0.00);
+//        request.setBalance(0.00);
+//        CreateWalletResponse response = walletService.createWallet(request);
+//        assertNotNull(response);
+//        assertNotNull(response.getId());
+    }
 
-// }
+}
